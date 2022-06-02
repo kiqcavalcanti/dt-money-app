69fec0f8a9c4e725aec246c0c7f17a302ef0e226
Kaique Cavalcanti <kaique.cavalcanti@doc88.com.br>
2022-06-01 23:25:04 -0300

    configurando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import { GlobalStyle } from "./styles/global";
+
+const Title = styled.h1`
+    color: #8257e6;
+    font-size: 64px;
+`
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Title>Hello World</Title>
+        <GlobalStyle/>
     </div>
   );
 }
